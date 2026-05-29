a83e4a7132d10daa39c2e62f072ee402afba3495
zchenytt <zchenytt@gmail.com>
2026-05-29 13:12:53 +0800

    Update MakieCode.jl

--- src/MakieCode.jl
+++ src/MakieCode.jl
@@ -1,41 +1,31 @@
-#######################################################
-
 include("src/WindGen.jl")
 using CairoMakie
-fig = Figure(size = (1000, 400));
-left = fig[1:4, 1] = GridLayout();
-axs_left = [Axis(left[i, j]) for i = 1:4, j = 1:2];
-axs_left[1, 1].title = "Single Scenario"
-axs_left[1, 2].title = "Single Scenario"
-right = fig[1:4, 2] = GridLayout();
-ax_r1 = Axis(right[1:2, 1]);
-ax_r1.title = "200 Scenarios, small variance"
-ax_r2 = Axis(right[3:4, 1]);
-ax_r2.title = "200 Scenarios, large variance"
-colgap!(fig.layout, 1, 20)  # gap between left and right blocks
-rowgap!(left, 10)
-rowgap!(right, 10)
-fig
-include("src/WindGen.jl")
-S = 200
+fig = Figure(size = (500, 600));
+ax11 = Axis(fig[1, 1], xticks=0:2:23, xticklabelsvisible=false, xticksvisible=false, yticksvisible=false, title = "Single Scenario 1,2");
+ax12 = Axis(fig[1, 2], xticks=0:2:23, xticklabelsvisible=false, xticksvisible=false, yticksvisible=false, title = "Single Scenario 3,4");
+ax21 = Axis(fig[2, 1], xticks=0:2:23, xticksvisible=false, yticksvisible=false, title = "Scenario", titlevisible=false);
+ax22 = Axis(fig[2, 2], xticks=0:2:23, xticksvisible=false, yticksvisible=false, title = "Scenario", titlevisible=false);
+ax31 = Axis(fig[3, 1:2], xticks=0:1:23, xticklabelsvisible=false, xticksvisible=false, yticksvisible=false, title = "500 samples - Smaller Variance");
+ax41 = Axis(fig[4, 1:2], xticks=0:1:23, xticksvisible=false, yticksvisible=false, title = "500 samples - Larger Variance");
+S = 4
 m = WindGen._S(S, 1);
-for i = 1:S
-    lines!(ax_r2, 1:24, m[i][:, 1], color=:red)
-    lines!(ax_r2, 1:24, m[i][:, 2], color=:blue)
-    lines!(ax_r2, 1:24, m[i][:, 3], color=:green)
+lines!(ax11, 0:23, m[1][:, 1], color=:red)
+lines!(ax11, 0:23, m[1][:, 2], color=:blue)
+lines!(ax11, 0:23, m[1][:, 3], color=:green)
+lines!(ax12, 0:23, m[2][:, 1], color=:red)
+lines!(ax12, 0:23, m[2][:, 2], color=:blue)
+lines!(ax12, 0:23, m[2][:, 3], color=:green)
+lines!(ax21, 0:23, m[3][:, 1], color=:red)
+lines!(ax21, 0:23, m[3][:, 2], color=:blue)
+lines!(ax21, 0:23, m[3][:, 3], color=:green)
+lines!(ax22, 0:23, m[4][:, 1], color=:red)
+lines!(ax22, 0:23, m[4][:, 2], color=:blue)
+lines!(ax22, 0:23, m[4][:, 3], color=:green)
+S = 500
+m = WindGen._S(S, 1);
+for i=1:S
+    lines!(ax41, 0:23, m[i][:, 1], color=:red, linewidth=0.1)
+    lines!(ax41, 0:23, m[i][:, 2], color=:blue, linewidth=0.1)
+    lines!(ax41, 0:23, m[i][:, 3], color=:green, linewidth=0.1)
 end
 fig
-S = 8;
-m = WindGen._S(S, 1);
-i = 0;
-for ax = axs_left
-    i += 1
-    lines!(ax, 1:24, m[i][:, 1], color=:red)
-    lines!(ax, 1:24, m[i][:, 2], color=:blue)
-    lines!(ax, 1:24, m[i][:, 3], color=:green)
-end
-ax_r2.xlabel = "time (h)"
-axs_left[4, 1].xlabel = "time (h)"
-axs_left[4, 2].xlabel = "time (h)"
-
-#######################################################
