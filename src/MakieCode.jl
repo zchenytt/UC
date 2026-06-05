f0fd96c66feee2eb6d2819149bcfafac042a9eec
zchenytt <zchenytt@gmail.com>
2026-06-06 07:39:40 +0800

    Update MakieCode.jl

--- src/MakieCode.jl
+++ src/MakieCode.jl
@@ -29,3 +29,17 @@ for i=1:S
     lines!(ax41, 0:23, m[i][:, 3], color=:green, linewidth=0.1)
 end
 fig
+
+
+
+using JLD2
+# @load "myvec.jld2" timensinalgo3mst lbv mstreopttime timensinalgo3 subtermigap
+using CairoMakie
+f = Figure(size=(680,400), figure_padding = 0.01);
+ax1 = Axis(f[1, 1], ylabel = "Master Lower Bound", ylabelcolor=:blue, yticklabelcolor = :blue, xticksvisible=false, xticklabelsvisible=false);
+ax2 = Axis(f[1, 1], ylabelcolor=:green, ylabel = "Master Reoptimize Time (s)", yticklabelcolor = :green, yaxisposition = :right);hidespines!(ax2);hidexdecorations!(ax2);
+ax3 = Axis(f[2, 1]; yscale=log10, ylabel = "Subproblem Terminating Gap", xlabel="Runtime of Algorithm 3 (s)", xticksvisible=false);
+lines!(ax1, timensinalgo3mst, lbv, color = :blue);
+scatter!(ax2, timensinalgo3mst, mstreopttime, color = :green, markersize=4);
+scatter!(ax3, timensinalgo3, subtermigap, markersize=3, color = :tomato);
+f
